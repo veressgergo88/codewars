2f34c313cc168dc670cd6bb6732281619b67d98e
veressgergo88 <veress.gergo88@gmail.com>
2023-10-30 13:51:09 +0100

    script27.ts added but not finished yet

---
+++ script27.ts
@@ -0,0 +1,25 @@
+export function persistence(num: number): number {
+    let str: string = "" + num
+    let count:number = 0
+
+    if (str.length === 1) {
+        return count = 0
+    } else {
+        for (let i = 0; i < str.length; i++) {
+            
+        }
+    }
+
+    return count
+}
+
+   let result1: number = persistence(39)
+   let result2: number = persistence(999)
+   let result3: number = persistence(4)
+   
+   console.log(result1)
+   console.log(result2)
+   console.log(result3)
+   
+       
+   export {}
